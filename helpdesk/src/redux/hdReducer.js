d7340c5f74e2be08041b546d9609d2df93b83824
Alex Whitt <whitta14@gmail.com>
2020-09-16 18:55:02 -0400

    setup for reducer and installed missing dependencies

---
+++ helpdesk/src/redux/hdReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    data: {},
+}
+
+export const hdReducer = (state = initialState, action) => {
+    switch(action){
+        default:
+            return state;
+
+    }
+}

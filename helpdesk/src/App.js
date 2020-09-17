bee4e1f900bcebc4a1ed8e9c9f699ad8fbd5e78b
jalvaradoWD <josealvarado1337@gmail.com>
2020-09-16 20:11:04 -0500

    Removed the TicketCreationForm Component, but kept the import

--- helpdesk/src/App.js
+++ helpdesk/src/App.js
@@ -3,11 +3,7 @@ import "./App.css";
 import TicketCreationForm from "./components/TicketCreationForm";
 
 function App() {
-  return (
-    <div className="App">
-      <TicketCreationForm />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

f947eda4c0e4bb4b80abd536f7034b8711530253
jalvaradoWD <josealvarado1337@gmail.com>
2020-09-16 19:59:16 -0500

    Rendered the component in the App Component

--- helpdesk/src/App.js
+++ helpdesk/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TicketList from "./components/TicketList";
 
 function App() {
   return (
     <div className="App">
-     
+      <TicketList />
     </div>
   );
 }

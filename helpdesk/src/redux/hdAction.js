f693b0ca8f2f5e8cfee26007b98d0bf6d06d06a6
Alex Whitt <whitta14@gmail.com>
2020-09-17 16:56:57 -0400

    fixed error with hook

--- helpdesk/src/redux/hdAction.js
+++ helpdesk/src/redux/hdAction.js
@@ -5,7 +5,8 @@ export const userLogin = (user) => dispatch => {
     dispatch({type: 'USER_LOGIN'});
     axios.post('https://areallyuniquename.herokuapp.com/api/users')
     .then(res => {
-        dispatch({type: 'LOGIN_SUCCSESS', payload: res.data});
+        dispatch({type: 'LOGIN_SUCCSESS'});
+        localStorage.setItem('token', res.data.payload);
     })
     .catch(er => {
         dispatch({type: 'LOGIN_FAIL', payload: 'incorrect username and password combination'})

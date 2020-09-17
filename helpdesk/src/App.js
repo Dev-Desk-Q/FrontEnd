6dabbe66b657edb1e05e4c4eb250b926a143b531
jalvaradoWD <josealvarado1337@gmail.com>
2020-09-16 19:48:56 -0500

    Added dummy data to the Ticket component

--- helpdesk/src/App.js
+++ helpdesk/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Ticket from "./components/Ticket";
 
 function App() {
   return (
     <div className="App">
-     
+      <Ticket />
     </div>
   );
 }

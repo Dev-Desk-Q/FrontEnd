df28706727ff68d530485ffc7b9488b91ade9551
jalvaradoWD <josealvarado1337@gmail.com>
2020-09-16 19:21:14 -0500

    Added onChange event listeners to each input tag.
    All information within the form is stored in the Form's state.

--- helpdesk/src/components/RegistrationForm.js
+++ helpdesk/src/components/RegistrationForm.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RegistrationForm = () => {
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const onChange = (e) => {
+    e.preventDefault();
+
+    const targetValue = e.target.value;
+    const targetName = e.target.name;
+
+    setFormState({ ...formState, [targetName]: targetValue });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formState);
+  };
+
   return (
     <>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <h2>Registeration Form</h2>
 
         <div className="form-group">
           <label htmlFor="username">Username</label>
           <br />
-          <input name="username" type="text" />
+          <input onChange={onChange} name="username" type="text" />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email</label>
           <br />
-          <input name="email" type="email" />
+          <input onChange={onChange} name="email" type="email" />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
           <br />
-          <input name="password" type="password" />
+          <input onChange={onChange} name="password" type="password" />
         </div>
+        <input type="submit" value="Register" />
       </form>
     </>
   );

99df377cf7893b57474e5b45304d1786bccd806e
jalvaradoWD <josealvarado1337@gmail.com>
2020-09-20 18:02:27 -0500

    Added a new input for password confirmation.
    Also added all the forms to be required to fill out.

--- helpdesk/src/components/RegistrationForm.js
+++ helpdesk/src/components/RegistrationForm.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import NavLogin from './navLogin';
-import { connect } from 'react-redux';
-import { regUser } from '../redux/hdAction'
+import NavLogin from "./navLogin";
+import { connect } from "react-redux";
+import { regUser } from "../redux/hdAction";
 
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
-
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
 const RegistrationForm = (props) => {
   const [formState, setFormState] = useState({
     username: "",
     password: "",
+    confirmPassword: "",
     role: "",
   });
 
@@ -26,54 +26,73 @@ const RegistrationForm = (props) => {
     e.preventDefault();
     props.regUser(formState);
   };
-  if (props.newUser === null){
-  return (
-    <>
-      <NavLogin info={props}/>
-      <Form onSubmit={onSubmit}>
-        <h2>Registeration Form</h2>
 
-        <FormGroup className="form-group">
-          <Label htmlFor="username">Username</Label>
-          <br />
-          <Input onChange={onChange} name="username" type="text" />
-        </FormGroup>
-        <FormGroup className="form-group">
-          <Label htmlFor="password">Password</Label>
-          <br />
-          <Input onChange={onChange} name="password" type="password" />
-          <br/>
-        </FormGroup>
-        <FormGroup className="form-group">
-          <Label htmlFor="role">Role</Label>
-          <br />
-          <Input type="select" onChange={onChange} name="role">
-            <option disabled={formState.role === "" ? false : true} value="">
-              -- Select a Role --
-            </option>
-            <option value="student">Student</option>
-            <option value="helper">Helper</option>
-          </Input>
-        </FormGroup>
-        <Input type="submit" value="Register" />
-      </Form>
-    </>
-  );}
-  else {
-    return(
+  if (props.newUser === null) {
+    return (
+      <>
+        <NavLogin info={props} />
+        <Form onSubmit={onSubmit}>
+          <h2>Registeration Form</h2>
+
+          <FormGroup className="form-group">
+            <Label htmlFor="username">Username</Label>
+            <br />
+            <Input onChange={onChange} name="username" type="text" required />
+          </FormGroup>
+          <FormGroup className="form-group">
+            <Label htmlFor="password">Password</Label>
+            <br />
+            <Input
+              onChange={onChange}
+              name="password"
+              type="password"
+              required
+              minLength="6"
+            />
+            <br />
+          </FormGroup>
+          <FormGroup className="form-group">
+            <Label htmlFor="confirmPassword">Confirm Password</Label>
+            <br />
+            <Input
+              onChange={onChange}
+              name="confirmPassword"
+              type="password"
+              minLength="6"
+              required
+            />
+            <br />
+          </FormGroup>
+          <FormGroup className="form-group">
+            <Label htmlFor="role">Role</Label>
+            <br />
+            <Input type="select" onChange={onChange} name="role" required>
+              <option disabled={formState.role === "" ? false : true} value="">
+                -- Select a Role --
+              </option>
+              <option value="student">Student</option>
+              <option value="helper">Helper</option>
+            </Input>
+          </FormGroup>
+          <Input type="submit" value="Register" />
+        </Form>
+      </>
+    );
+  } else {
+    return (
       <div>
         <p> Please log in {props.newUser}</p>
       </div>
-    )
+    );
   }
 };
 
-const stp = state => {
+const stp = (state) => {
   return {
-      newUser: state.registered,
-  }
-}
+    newUser: state.registered,
+  };
+};
 
-const dtp = { regUser }
+const dtp = { regUser };
 
-export default connect(stp,dtp)(RegistrationForm);
+export default connect(stp, dtp)(RegistrationForm);

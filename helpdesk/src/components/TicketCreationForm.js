5dec8be9f80154e7ea83acdee17d492d382ca70c
jalvaradoWD <josealvarado1337@gmail.com>
2020-09-22 19:01:59 -0500

    Adjusted the styles of the ticket creation form for the ticket creation form component.

--- helpdesk/src/components/TicketCreationForm.js
+++ helpdesk/src/components/TicketCreationForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import NavLogin from './navLogin';
 import { connect } from 'react-redux';
-import { Form, Input, Label, FormGroup } from "reactstrap";
+import { Form, Input, Label, FormGroup, Container } from "reactstrap";
 import { axiosWithAuth } from '../utils/axiosAuth';
 
 const TicketCreationForm = (props) => {
@@ -39,46 +39,48 @@ const TicketCreationForm = (props) => {
   return (
     <>
       <NavLogin props={props} />
-      <Form onSubmit={onSubmit}>
-        <h3>Ticket Creation</h3>
-        <FormGroup className="form-group">
-          <Label htmlFor="title">Title</Label>
-          <br />
-          <Input onChange={onChange} name="title" type="text" />
-        </FormGroup>
-        <FormGroup className="form-group">
-          <Label htmlFor="description">Description</Label>
-          <br />
-          <Input
-            rows="4"
-            cols="50"
-            type="textarea"
-            onChange={onChange}
-            name="description"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="tried">What has been tried</Label>
-          <br />
-          <Input
-            onChange={onChange}
-            rows="4"
-            cols="50"
-            type="textarea"
-            name="tried"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor="category">Category</Label>
-          <br />
-          <Input onChange={onChange} type="select" name="category">
-            <option value="option1">option1</option>
-            <option value="option2">option2</option>
-            <option value="option3">option3</option>
-          </Input>
-        </FormGroup>
-        <Input type="submit" value="Post Ticket" />
-      </Form>
+      <Container>
+        <Form onSubmit={onSubmit}>
+          <h3>Ticket Creation</h3>
+          <FormGroup className="form-group">
+            <Label htmlFor="title">Title</Label>
+            <br />
+            <Input onChange={onChange} name="title" type="text" />
+          </FormGroup>
+          <FormGroup className="form-group">
+            <Label htmlFor="description">Description</Label>
+            <br />
+            <Input
+              rows="4"
+              cols="50"
+              type="textarea"
+              onChange={onChange}
+              name="description"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="tried">What has been tried</Label>
+            <br />
+            <Input
+              onChange={onChange}
+              rows="4"
+              cols="50"
+              type="textarea"
+              name="tried"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="category">Category</Label>
+            <br />
+            <Input onChange={onChange} type="select" name="category">
+              <option value="option1">option1</option>
+              <option value="option2">option2</option>
+              <option value="option3">option3</option>
+            </Input>
+          </FormGroup>
+          <Input type="submit" value="Post Ticket" />
+        </Form>
+      </Container>
     </>
   );
 };
